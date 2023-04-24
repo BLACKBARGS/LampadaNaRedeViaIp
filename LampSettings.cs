f7bf734123c0142690896f0db92b8e189e24ee13
Francys Rodrigues <119719373+BLACKBARGS@users.noreply.github.com>
2023-04-24 14:59:13 -0300

    modified:   LampSettings.cs 	modified:   obj/Debug/net7.0/LampadaNaRedeViaIp.GeneratedMSBuildEditorConfig.editorconfig 	modified:   obj/Debug/net7.0/LampadaNaRedeViaIp.assets.cache

--- LampSettings.cs
+++ LampSettings.cs
@@ -2,10 +2,10 @@ internal class LampSettings
 {
     public LampSettings(){}
 
-    public string IP { get; set; }
-    public string MAC { get; set; }
+    public string? IP { get; set; }
+    public string? MAC { get; set; }
     public int Port { get; set; }
     public bool IsOn { get; set; }
-    public string Color { get; set; }
+    public string? Color { get; set; }
     public int Brightness { get; set; }
 }
